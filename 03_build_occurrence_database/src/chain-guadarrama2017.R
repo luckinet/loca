5c43eabe3828df7232779a07f036375d24a8282b
Peter <peter.pothmann@idiv.de>
2022-11-02 08:45:16 +0100

    time periods missing

--- 03_build_occurrence_database/src/chain-guadarrama2017.R
+++ 03_build_occurrence_database/src/chain-guadarrama2017.R
@@ -1,19 +1,41 @@
 # script arguments ----
 #
 thisDataset <- "Chain-Guadarrama2017"
-thisPath <- paste0(DBDir, thisDataset, "/")
-
+thisPath <- paste0(occurrenceDBDir, thisDataset, "/")
+assertDirectoryExists(x = thisPath)
+message("\n---- ", thisDataset, " ----")
 
 # reference ----
 #
 bib <- bibtex_reader(paste0(thisPath, "pericles_1600058741.bib")) # choose between ris_reader() or bibtex_reader()
 
+# column         type            description
+# name
+# description    [character]   description of the data-set
+# url            [character]   ideally the doi, but if it doesn't have one, the
+#                              main source of the database
+# donwload_date  [POSIXct]     the date (DD-MM-YYYY) on which the data-set was
+#                              downloaded
+# type           [character]   "dynamic" (when the data-set updates regularly)
+#                              or "static"
+# license        [character]   abbreviation of the license under which the
+#                              data-set is published
+# contact        [character]   if it's a paper that should be "see corresponding
+#                              author", otherwise some listed contact
+# disclosed      [logical]
+# bibliography   [handl]       bibliography object from the 'handlr' package
+# path           [character]   the path to the occurrenceDB
+
+description <- "Quantifying relationships between plant functional traits and abiotic gradients is valuable for evaluating potential responses of forest communities to climate change. However, the trajectories of change expected to occur in tropical forest functional characteristics as a function of future climate variation are largely unknown. We modeled community level trait values of Costa Rican rain forests as a function of current and future climate, and quantified potential changes in functional composition."
+url <- "https://doi.org/10.1111/ecog.02637"
+license <- "CC0 1.0"
+
 regDataset(name = thisDataset,
-           description = "Quantifying relationships between plant functional traits and abiotic gradients is valuable for evaluating potential responses of forest communities to climate change. However, the trajectories of change expected to occur in tropical forest functional characteristics as a function of future climate variation are largely unknown. We modeled community level trait values of Costa Rican rain forests as a function of current and future climate, and quantified potential changes in functional composition.",
-           url = "https://doi.org/10.1111/ecog.02637",
+           description = description,
+           url = url,
            download_date = "2021-08-10",
            type = "static",
-           licence = "CC0 1.0",
+           licence = licence,
            contact = "see corresponding author",
            disclosed = "yes",
            bibliography = bib,
@@ -25,73 +47,41 @@ regDataset(name = thisDataset,
 #
 data <- read_delim(paste0(thisPath, "observed CWM traits.csv"), delim = ";", n_max = 127)
 
-
-# manage ontology ---
-#
-# define labels in the new dataset and their matching already harmonised labels
-#matches <- tibble(new = c(...),
-#                  old = c(...))
-# getConcept(label_en = matches$old, missing = TRUE)
-
-#newConcept(new = c(),
-#           broader = c(), # the labels 'new' should be nested into
-#           class = , # try to keep that as conservative as possible and only come up with new classes, if really needed
-#           source = thisDataset)
-
-#getConcept(label_en = matches$old) %>%
-  # ... %>% apply some additional filters (optional)
-#  pull(label_en) %>%
-#  newMapping(concept = .,
-#             external = matches$new,
-#             match = , # in most cases that should be "close", see ?newMapping
-#             source = thisDataset,
-#             certainty = ) # value from 1:3
-
-
 # harmonise data ----
 #
-# carry out optional corrections and validations ...
 
-
-# ... and then reshape the input data into the harmonised format
 temp <- data %>%
   distinct(lat, long, .keep_all = TRUE) %>%
   mutate(
     datasetID = thisDataset,
     fid = row_number(),
-    type = "point", # "point" or "areal" (such as plot, region, nation, etc)
-    x = long, # x-value of centroid
-    y = lat, # y-value of centroid
-    year = NA, # dates are missing
-    month = NA_real_, # must be NA_real_ if it's not given
-    day = NA_integer_, # must be NA_integer_ if it's not given
-    country = NA_character_,
-    irrigated = NA, # in case the irrigation status is provided
-    area = NA, # in case the features are from plots and the table gives areas but no spatial object is available
-    presence = FALSE, # whether the data are 'presence' data (TRUE), or whether they are 'absence' data (i.e., that the data point indicates the value in externalValue is not present) (FALSE)
+    type = "point",
+    x = long,
+    y = lat,
+    date = NA,
+    country = "Costa Rica",
+    irrigated = NA,
+    area = NA,
+    presence = FALSE,
     externalID = PLOT,
     externalValue = "Undisturbed Forest",
     LC1_orig = NA_character_,
     LC2_orig = NA_character_,
     LC3_orig = NA_character_,
-    sample_type = "field", # "field", "visual interpretation", "experience", "meta study" or "modelled"
-    collector = "expert", # "expert", "citizen scientist" or "student"
-    purpose = "study", # "monitoring", "validation", "study" or "map development"
+    sample_type = "field",
+    collector = "expert",
+    purpose = "study",
     epsg = 4326) %>%
-  select(datasetID, fid, country, x, y, epsg, type, year, month, day, irrigated, area, presence, externalID, externalValue, LC1_orig, LC2_orig, LC3_orig, sample_type, collector, purpose, everything())
-
-# In case we are dealing with areal data, build object that contains polygons
-# temp_sf <- temp %>%
-#   mutate(geom = ) %>% # select the geometry object
-#   select(datasetID, fid, geom)
+  select(datasetID, fid, type, country, x, y, geometry, epsg, area, date,
+         externalID, externalValue, irrigated,presence, LC1_orig, LC2_orig,
+         LC3_orig, sample_type, collector, purpose, everything())
 
 
 # write output ----
 #
-validateFormat(object = temp, type = "in-situ point") %>%
-  saveDataset(dataset = thisDataset)
+validateFormat(object = temp) %>%
+  saveDataset(path = occurrenceDBDir, name = thisDataset)
 
-# validateFormat(object = temp_sf, type = "in-situ areal") %>%
-#   write_sf(dsn = paste0(thisDataset, "_sf.gpkg"), delete_layer = TRUE)
+write_rds(x = luckiOnto, file = paste0(dataDir, "tables/luckiOnto.rds"))
 
 message("\n---- done ----")
