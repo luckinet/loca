265ea1203f590bb8f49ca32ed4f12918b2fd2bc3
EhrmannS <steffen.ehrmann@posteo.de>
2023-10-08 23:53:18 +0200

    - tentatively include two old functions - remove "load_filenames, as the paths are now handled via boot.R

--- 01_load_functions.R
+++ 01_load_functions.R
@@ -230,9 +230,17 @@ start_gridDB <- function(root = NULL){
   # test whether the required directories exist and create them if they don't exist
   if(!testDirectory(x = root, access = "rw")){
     dir.create(file.path(root))
-    dir.create(file.path(root, "input"))
-    dir.create(file.path(root, "processed"))
     message("I have created a new project directory.")
+  }
+  if(!testDirectory(x = root, access = "rw")){
+    dir.create(file.path(root))
+    message("I have created a new project directory.")
+  }
+  if(!testDirectory(x = file.path(root, "stage1"), access = "rw")){
+    dir.create(file.path(root, "stage1"))
+  }
+  if(!testDirectory(x = file.path(root, "stage3"), access = "rw")){
+    dir.create(file.path(root, "stage3"))
   }
 
   # create the empty inventory tables, if they don't exist yet
@@ -451,32 +459,6 @@ regDataset <- function(name = NULL, description = NULL, type = NULL,
 
   write_csv(x = out, file = paste0(path, "inv_datasets.csv"), na = "")
   return(temp)
-
-}
-
-
-# Build file names ----
-#
-# profile  [list]  the profile as derived via load_profile().
-
-load_filenames <- function(profile){
-
-  root <- profile$dir
-  assertDirectoryExists(x = root, access = "rw")
-
-  wF <- read_csv(file = workingFiles, col_types = c("ccnlcciii"))
-
-  gadmVect <- list(gadm = paste0(dirname(dirname(root)), "/misc/gadm36_levels.gpkg"))
-
-  out <- map(.x = seq_along(wF$name), .f = function(ix){
-    temp <- wF[ix,]
-    paste0(root, temp$folder, "/", temp$name, "_", profile$name, "_", profile$version, ".", temp$type)
-  })
-  names(out) <- wF$name
-
-  out <- c(gadmVect, out)
-
-  return(out)
 
 }
 
@@ -704,5 +686,426 @@ saveDataset <- function(object, path, name, outType = "rds"){
   } else {
     saveRDS(object = object, file = paste0(thisPath, ".rds"))
   }
+
+}
+
+# generate input data for a LUCKINet module ----
+#
+# module       [character]  the module of the LUCKINet workflow for which to
+#                           generate input data.
+# dir          [character]  the directory where to store the model run.
+# landcover    [integer]    number of landcover classes.
+# landuse      [integer]    number of landuse classes.
+# territories  [integer]    number of territories.
+# dVal         [numeric]    manual values of demand areas. Overrides {ddL}
+#                           and {ddT}.
+# ddL          [numeric]    the demand distribution across landuse classes. Must
+#                           contain as many values as there are landuse classes
+#                           with values between 0 and 1, which add up to 1. If
+#                           unspecified, this will be 1/landuse.
+# ddT          [numeric]    the demand distribution across territories. Must
+#                           contain as many values as there are territories with
+#                           values between 0 and 1, which add up to 1. If
+#                           unspecified, this will be 1/territories.
+# adT          [character]  the spatial/area distribution of territories.
+#                           Currently supported values are "diagonal" and
+#                           "horizontal".
+# app          [numeric]    area per pixel; multiplied with all area related
+#                           measures.
+# withUrban    [logical]    include an urban class?
+# withRestr    [logical]    include restricted areas?
+# seed         [integer]    seed for generating randomness (for instance with
+#                           suitability maps).
+
+generate_input <- function(module, dir, landcover, landuse, territories,
+                           dVal = NULL, ddL = NULL, ddT = NULL, adT, app = 100,
+                           withUrban, withRestr,
+                           seed = 1137){
+
+  message("revise functionality")
+
+  # # library(luckiTools); library(terra); library(checkmate); library(NLMR); library(tibble); library(purrr); library(dplyr)
+  # # module = "initial landuse"; dir = dataDir; landcover = 2; landuse = 2; territories = 1; ddL = NULL; ddT = NULL; dVal = NULL; adT = "horizontal"; withUrban = FALSE; withRestr = TRUE; seed = 1137
+  #
+  # # include:
+  # #   - over/underestimate areas of classes
+  # #   -
+  #
+  # # test input ----
+  # assertChoice(x = module, choices = c("suitability", "initial landuse", "allocation"))
+  # assertDirectoryExists(x = dir, access = "rw")
+  # assertIntegerish(x = landcover, lower = 1, any.missing = FALSE, len = 1)
+  # assertIntegerish(x = landuse, lower = 1, any.missing = FALSE, len = 1)
+  # assertIntegerish(x = territories, lower = 1, any.missing = FALSE, len = 1)
+  # assertNumeric(x = dVal, len = landuse*territories, null.ok = TRUE)
+  # assertChoice(x = adT, choices = c("diagonal", "horizontal"))
+  # assertLogical(x = withUrban, any.missing = FALSE, len = 1)
+  # assertLogical(x = withRestr, any.missing = FALSE, len = 1)
+  # assertIntegerish(x = seed, any.missing = FALSE, len = 1)
+  #
+  # theDir <- paste0(dir, "run/testing_0.1.0/")
+  # if(testDirectoryExists(x = theDir)){
+  #   unlink(theDir, recursive = TRUE)
+  # }
+  #
+  #
+  # # manage profile ----
+  # covNames <- c(
+  #   "iGHS_POP/GHSP"
+  # )
+  # params <- list(years = c(2000:2010),
+  #                pixel_size = c(1, 1),
+  #                arealDB_dir = "censusDB_testing",
+  #                arealDB_extent = "Estonia",
+  #                pointDB_dir = "pointDB_testing",
+  #                pointDB_extent = "Estonia",
+  #                landcover = "CCI_landCover/landCover",
+  #                predictors = covNames)
+  # write_profile(root = dir, name = "testing", version = "0.1.0", parameters = params)
+  # profile <- load_profile(root = dir, name = "testing", version = "0.1.0")
+  #
+  # set.seed(seed = seed)
+  #
+  # # build stats and maps ----
+  # mp_cover <- rast(nrows = 10, ncols = 10, xmin = 0, xmax = 10, ymin = 0, ymax = 10)
+  #
+  # if(landcover == 1){
+  #
+  #   mp_cover[] <- 10
+  #
+  # } else if(landcover == 2){
+  #
+  #   mp_cover[] <- c(rep(c(10, 10, 10, 10, 10, 20, 20, 20, 20, 20), 10))
+  #
+  # } else {
+  #   stop("more than 2 landcover classes are currently not supported")
+  # }
+  #
+  # mp_ahID <- mp_mask <- mp_rest <- mp_area <- mp_cover
+  #
+  # if(landuse == 1){
+  #
+  #   theClasses <- c(1120)
+  #   theNames <- c("Cropland")
+  #
+  #   # layer1 <- rast(nlm_planargradient(ncol = 10, nrow = 10, resolution = 1, direction = 225))
+  #   # find replacement for nlm_planargradient
+  #   layer1Name <- "1120"
+  #
+  # } else if(landuse == 2){
+  #
+  #   theClasses <- c(1120, 1122)
+  #   theNames <- c("Cropland", "Forest land")
+  #
+  #   # layer1 <- rast(nlm_planargradient(ncol = 10, nrow = 10, resolution = 1, direction = 225))
+  #   # find replacement for nlm_planargradient
+  #   layer2 <- 1 - layer1
+  #
+  #   layer1Name <- "1120"
+  #   layer2Name <- "1122"
+  #
+  # } else if(landuse == 3){
+  #
+  #   theClasses <- c(1120, 1122, 1124)
+  #   theNames <- c("Cropland", "Forest land", "Grazing land")
+  #
+  #   # layer1 <- rast(nlm_planargradient(ncol = 10, nrow = 10, resolution = 1, direction = 180)) * 2/3
+  #   # find replacement for nlm_planargradient
+  #   layer1[1:10] <- 0
+  #   # layer2 <- rast(nlm_random(ncol = 10, nrow = 10)/3)
+  #   # find replacement for nlm_random
+  #   layer3 <- 1 - (layer1 + layer2)
+  #
+  #   layer1Name <- "1120"
+  #   layer2Name <- "1122"
+  #   layer3Name <- "1124"
+  #
+  # } else {
+  #   stop("more than 3 landuse classes are currently not supported")
+  # }
+  #
+  # if(is.null(ddL)){
+  #   theArea <- rep(1, landuse) / landuse
+  # } else {
+  #   assertNumeric(x = ddL, lower = 0, upper = 1, len = landuse)
+  #   assertTRUE(sum(ddL) == 1)
+  #   theArea <- ddL
+  # }
+  #
+  # tempClasses <- tibble(luckinetID = theClasses,
+  #                       term = theNames,
+  #                       area = theArea)
+  #
+  # if(territories == 1){
+  #
+  #   theTerritories <- 70
+  #   mp_ahID[] <- 70
+  #   # ahID_areas <- tibble(ahID = 70, total_area = 900)
+  #
+  # } else if(territories == 2){
+  #
+  #   theTerritories <- c(70, 80)
+  #
+  #   if(adT == "diagonal"){
+  #     mp_ahID[] <- c(rep(70, 18), rep(80, 2),    # two equally sized units
+  #                    rep(70, 8), rep(80, 2),
+  #                    rep(70, 6), rep(80, 4),
+  #                    rep(70, 6), rep(80, 4),
+  #                    rep(70, 4), rep(80, 6),
+  #                    rep(70, 3), rep(80, 7),
+  #                    rep(70, 3), rep(80, 7),
+  #                    rep(70, 1), rep(80, 9),
+  #                    rep(70, 1), rep(80, 9))
+  #   } else if(adT == "horizontal"){
+  #     mp_ahID[] <- c(rep(70, 50), rep(80, 50))
+  #   }
+  #
+  #   # ahID_areas <- tibble(ahID = c(70, 80), total_area = c(450, 450))
+  #
+  # } else {
+  #   stop("more than 2 territories are currently not supported")
+  # }
+  # names(mp_ahID) <- "ahID"
+  # names(mp_cover) <- "cover"
+  #
+  # # region mask
+  # mp_mask[] <- 1
+  # mp_mask[c(7, 8, 9, 10, 20, 90, 91, 98, 99, 100)] <- NA
+  # names(mp_mask) <- "mask"
+  #
+  # # area map
+  # mp_area[] <- app
+  # names(mp_area) <- "area"
+  #
+  # # map of ahID and landcover
+  # mp_temp <- mp_ahID * as.numeric(paste0(1, paste0(rep(0, nchar(max(values(mp_cover)))), collapse = "")))
+  # mp_temp <- mp_temp + mp_cover
+  #
+  # if(is.null(ddT)){
+  #   theArea <- rep(theArea, territories) / territories
+  # } else {
+  #   assertNumeric(x = ddT, lower = 0, upper = 1, len = territories)
+  #   assertTRUE(sum(ddT) == 1)
+  #   theArea <- as.vector(matrix(theArea, length(theArea), 1) %*% ddT)
+  # }
+  #
+  # tempTerritories <- tibble(geoID = 1, year = 2000,
+  #                           nation = "Estonia",
+  #                           ahID = theTerritories,
+  #                           ahLevel = 1)
+  #
+  # comb <- expand.grid(theTerritories, theClasses) %>%
+  #   as_tibble() %>%
+  #   set_names("ahID", "luckinetID")
+  #
+  # census <- tempTerritories %>%
+  #   left_join(comb, by = "ahID") %>%
+  #   left_join(tempClasses, by = "luckinetID") %>%
+  #   mutate(area = theArea * sum(values(mp_area)))
+  #
+  #
+  # # handle special cases ----
+  # if(withRestr){
+  #   # mp_rest[] <- abs(rnorm(length(values(mp_area)), 0, 0.03))
+  #   mp_rest[] <- rep(c(0.02, 0.01, 0.02, 0.01, 0.02, 0.01, 0.02, 0.01, 0.02, 0.01, 0.01, 0.02, 0.01, 0.02, 0.01, 0.02, 0.01, 0.02, 0.01, 0.02), 5)
+  #   if(territories == 2 & landuse == 2){
+  #     census$area <- c(2199299, 2196409, 2182832, 2186333)
+  #   } else if(territories == 1 & landuse == 2){
+  #     census$area <- c(4432500, 4432500)
+  #   }
+  # } else {
+  #   mp_rest[] <- 0
+  # }
+  # names(mp_rest) <- "restricted"
+  #
+  # if(withUrban){
+  #
+  #   mp_cover[c(72, 73, 82, 83)] <- 40
+  #
+  #   if(territories == 2 & landuse == 2){
+  #     newClasses <- tibble(geoID = 1, year = 2000, nation = "Estonia",
+  #                          ahID = theTerritories, ahLevel = 1,
+  #                          luckinetID = 1126, term = "Other land")
+  #     census <- bind_rows(census, newClasses) %>%
+  #       arrange(ahID, luckinetID)
+  #     census$area <- c(2474644.4, 1742230.6, 180000, 1568803.7, 2628195.5, 180000)
+  #
+  #     layer3 <- layer1
+  #     layer3[] <- 0
+  #     layer3[c(72, 73, 82, 83)] <- c(0.9, 0.95, 0.99, 0.95)
+  #     layer1[c(72, 73, 82, 83)] <- 0
+  #     layer2[c(72, 73, 82, 83)] <- c(0.1, 0.05, 0.01, 0.05)
+  #
+  #     mp_rest[c(72, 73, 82, 83)] <- 0
+  #
+  #     theClasses <- c(theClasses, 1126)
+  #     layer3Name <- "1126"
+  #
+  #   } else if(territories == 1 & landuse == 2){
+  #
+  #   }
+  #
+  # }
+  #
+  # if(!is.null(dVal)){
+  #   census$area <- dVal
+  # }
+  #
+  #
+  # # landcover limits ----
+  # lc_limits <- tibble(landcover = rep(c("Cropland_lc", "Forest_lc", "Meadow_lc", "Other_lc"), each = 4),
+  #                     lcID = rep(c(10, 20, 30, 40), each = 4),
+  #                     luckinetID = rep(c(1120, 1122, 1124, 1126), 4),
+  #                     short = rep(c("crop", "forest", "grazing", "other"), 4),
+  #                     min = c(0.5, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0),
+  #                     max = c(1, 0.5, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 0.5, 1, 0.5, 0, 0, 0, 1))
+  #
+  # lc_limits <- lc_limits %>%
+  #   filter(luckinetID %in% theClasses)
+  #
+  # # tiles and geometries ----
+  # theTiles <- tibble(x = c(0, 10, 10, 0),
+  #                    y = c(0, 0, 10, 10)) %>%
+  #   gs_polygon() %>%
+  #   setFeatures(tibble(fid = 1, target = TRUE)) %>%
+  #   setCRS(crs = crs(mp_cover)) %>%
+  #   gc_sf()
+  #
+  #
+  # # write output ----
+  # saveRDS(object = census, file = paste0(profile$dir, "tables/census.rds"))
+  # saveRDS(object = lc_limits, paste0(profile$dir, "tables/landcover_limits.rds"))
+  #
+  # st_write(obj = theTiles, dsn = paste0(profile$dir, "visualise/tiles.gpkg"), quiet = TRUE)
+  #
+  # if(exists(x = "layer1")){
+  #
+  #   writeRaster(x = layer1,
+  #               filename = paste0(profile$dir, "suitability/suit_", layer1Name, "_2000_testing_0.1.0.tif"),
+  #               overwrite = TRUE,
+  #               filetype = "GTiff",
+  #               datatype = "FLT4S")
+  #
+  #   writeRaster(x = layer1,
+  #               filename = paste0(profile$dir, "tiles/suit_", layer1Name, "_2000_1_testing_0.1.0.tif"),
+  #               overwrite = TRUE,
+  #               filetype = "GTiff",
+  #               datatype = "FLT4S")
+  #
+  # }
+  #
+  # if(exists(x = "layer2")){
+  #
+  #   writeRaster(x = layer2,
+  #               filename = paste0(profile$dir, "suitability/suit_", layer2Name, "_2000_testing_0.1.0.tif"),
+  #               overwrite = TRUE,
+  #               filetype = "GTiff",
+  #               datatype = "FLT4S")
+  #
+  #   writeRaster(x = layer2,
+  #               filename = paste0(profile$dir, "tiles/suit_", layer2Name, "_2000_1_testing_0.1.0.tif"),
+  #               overwrite = TRUE,
+  #               filetype = "GTiff",
+  #               datatype = "FLT4S")
+  #
+  # }
+  #
+  # if(exists(x = "layer3")){
+  #
+  #   writeRaster(x = layer3,
+  #               filename = paste0(profile$dir, "tiles/suit_", layer3Name, "_2000_1_testing_0.1.0.tif"),
+  #               overwrite = TRUE,
+  #               filetype = "GTiff",
+  #               datatype = "FLT4S")
+  #
+  #   writeRaster(x = layer3,
+  #               filename = paste0(profile$dir, "suitability/suit_", layer3Name, "_2000_testing_0.1.0.tif"),
+  #               overwrite = TRUE,
+  #               filetype = "GTiff",
+  #               datatype = "FLT4S")
+  #
+  # }
+  #
+  # writeRaster(x = mp_cover,
+  #             filename = paste0(profile$dir, "landcover/landcover_2000_testing_0.1.0.tif"),
+  #             overwrite = TRUE,
+  #             filetype = "GTiff",
+  #             datatype = "FLT4S")
+  #
+  # writeRaster(x = mp_ahID,
+  #             filename = paste0(profile$dir, "maps/ahID_testing_0.1.0.tif"),
+  #             overwrite = TRUE,
+  #             filetype = "GTiff",
+  #             datatype = "FLT4S")
+  #
+  # writeRaster(x = mp_mask,
+  #             filename = paste0(profile$dir, "maps/regionMask_testing_0.1.0.tif"),
+  #             overwrite = TRUE,
+  #             filetype = "GTiff",
+  #             datatype = "FLT4S")
+  #
+  # writeRaster(x = mp_area,
+  #             filename = paste0(profile$dir, "maps/pixelArea_", profile$name, "_", profile$version, ".tif"),
+  #             overwrite = TRUE,
+  #             filetype = "GTiff",
+  #             datatype = "FLT4S")
+  #
+  # writeRaster(x = mp_rest,
+  #             filename = paste0(profile$dir, "maps/areaRestricted_testing_0.1.0.tif"),
+  #             overwrite = TRUE,
+  #             filetype = "GTiff",
+  #             datatype = "FLT4S")
+  #
+  # writeRaster(x = mp_temp,
+  #             filename = paste0(profile$dir, "maps/ahIDLC_testing_0.1.0.tif"),
+  #             overwrite = TRUE,
+  #             filetype = "GTiff",
+  #             datatype = "FLT4S")
+
+
+}
+
+# Make a difference from gridded objects of two model runs
+#
+# obj  [character]  name of the object to get a difference from several runs
+# x    [character]  the base run, from which to subtract {y}. If missing, the
+#                   object is taken from the most recent run.
+# y    [character]  the run that shall be subtracted from {x}.
+
+get_difference <- function(obj, x = NULL, y = NULL){
+
+  message("revise functionality")
+
+  # assertCharacter(x = obj, len = 1, any.missing = FALSE)
+  # assertClass(x = record, classes = "environment")
+  # assertChoice(x = x, choices = names(record), null.ok = TRUE)
+  # assertChoice(x = y, choices = names(record), null.ok = TRUE)
+  #
+  # if(is.null(x)){
+  #   origin <- get(obj)
+  #   x <- "current"
+  # } else{
+  #   origin <- record[[x]][[obj]]
+  # }
+  #
+  # if(is.null(y)){
+  #   ind <- ifelse(length(names(record)) == 1, 1, length(names(record)) - 1)
+  #   y <- names(record)[ind]
+  #   second <- record[[y]][[obj]]
+  # } else{
+  #   second <- record[[y]][[obj]]
+  # }
+  #
+  # if(dim(origin)[3] == 1){
+  #   out <- origin - second
+  # } else {
+  #   out <- origin
+  #   for(i in 1:dim(origin)[3]){
+  #     out[[i]] <- origin[[i]] - second[[i]]
+  #   }
+  # }
+  #
+  # names(out) <- paste0(names(origin), " diff(", x, "|", y, ")")
+  # return(out)
 
 }
