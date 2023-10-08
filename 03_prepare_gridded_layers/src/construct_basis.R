16544885ef429b15ad7b09570775a22e477dd890
EhrmannS <steffen.ehrmann@posteo.de>
2023-10-08 23:57:41 +0200

    new, simpler version of setting up the basic rasters

---
+++ 03_prepare_gridded_layers/src/construct_basis.R
@@ -0,0 +1,50 @@
+message("\n---- construct basic gridded layers ----")
+
+
+# derive template raster ----
+#
+message(" --> pixel template")
+world_template <- rast(res = profile$pixel_size[1], vals = 0)
+
+writeRaster(x = world_template,
+            filename = templ_pixels,
+            overwrite = TRUE,
+            filetype = "GTiff",
+            datatype = "INT1U",
+            gdal = c("COMPRESS=DEFLATE", "ZLEVEL=9", "PREDICTOR=2"))
+
+message(" --> model mask")
+model_mask <- crop(x = world_template, y = ext(profile$extent))
+values(model_mask) <- 1
+
+writeRaster(x = model_mask,
+            filename = mask_modelregion_path,
+            overwrite = TRUE,
+            filetype = "GTiff",
+            datatype = "INT1U",
+            gdal = c("COMPRESS=DEFLATE", "ZLEVEL=9", "PREDICTOR=2"))
+
+
+# derive pixel areas ----
+#
+message(" --> pixel areas")
+
+
+
+writeRaster(x = ,
+            filename = ,
+            overwrite = TRUE,
+            filetype = "GTiff",
+            datatype = "FLT4S",
+            gdal = c("COMPRESS=DEFLATE", "ZLEVEL=9", "PREDICTOR=2"))
+
+
+# setup gdal-vrt ----
+#
+message("\n---- setup virtual dataset ----")
+
+
+
+
+# beep(sound = 10)
+message("\n     ... done")
