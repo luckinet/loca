a181f16eba04607fb26e21ed9821254fbdbda5c8
EhrmannS <steffen.ehrmann@posteo.de>
2023-11-05 23:39:27 +0100

    remove "update"

--- 04_build_census_database/src/01_setup_gadm.R
+++ 04_build_census_database/src/01_setup_gadm.R
@@ -23,15 +23,13 @@ regDataseries(name = "gadm36",
               description = "Database of Global Administrative Areas v3.6",
               homepage = "https://gadm.org/index.html",
               licence_link = "https://gadm.org/license.html",
-              licence_path = "not available",
-              update = TRUE)
+              licence_path = "not available")
 
 regDataseries(name = "gadm41",
               description = "Database of Global Administrative Areas v4.1",
               homepage = "https://gadm.org/index.html",
               licence_link = "https://gadm.org/license.html",
-              licence_path = "not available",
-              update = TRUE)
+              licence_path = "not available")
 
 
 # register geometries ----
@@ -40,43 +38,37 @@ regGeometry(gSeries = "gadm36",
             label = list(al1 = "NAME_0"),
             archive = "gadm36_levels_gpkg.zip|gadm36_levels.gpkg",
             archiveLink = "https://geodata.ucdavis.edu/gadm/gadm4.1/gadm36_levels_gpkg.zip",
-            updateFrequency = "unknown",
-            update = TRUE)
+            updateFrequency = "unknown")
 
 regGeometry(gSeries = "gadm36",
             label = list(al1 = "NAME_0", al2 = "NAME_1"),
             archive = "gadm36_levels_gpkg.zip|gadm36_levels.gpkg",
             archiveLink = "https://geodata.ucdavis.edu/gadm/gadm4.1/gadm36_levels_gpkg.zip",
-            updateFrequency = "unknown",
-            update = TRUE)
+            updateFrequency = "unknown")
 
 regGeometry(gSeries = "gadm36",
             label = list(al1 = "NAME_0", al2 = "NAME_1", al3 = "NAME_2"),
             archive = "gadm36_levels_gpkg.zip|gadm36_levels.gpkg",
             archiveLink = "https://geodata.ucdavis.edu/gadm/gadm4.1/gadm36_levels_gpkg.zip",
-            updateFrequency = "unknown",
-            update = TRUE)
+            updateFrequency = "unknown")
 
 regGeometry(gSeries = "gadm36",
             label = list(al1 = "NAME_0", al2 = "NAME_1", al3 = "NAME_2", al4 = "NAME_3"),
             archive = "gadm36_levels_gpkg.zip|gadm36_levels.gpkg",
             archiveLink = "https://geodata.ucdavis.edu/gadm/gadm4.1/gadm36_levels_gpkg.zip",
-            updateFrequency = "unknown",
-            update = TRUE)
+            updateFrequency = "unknown")
 
 regGeometry(gSeries = "gadm36",
             label = list(al1 = "NAME_0", al2 = "NAME_1", al3 = "NAME_2", al4 = "NAME_3", al5 = "NAME_4"),
             archive = "gadm36_levels_gpkg.zip|gadm36_levels.gpkg",
             archiveLink = "https://geodata.ucdavis.edu/gadm/gadm4.1/gadm36_levels_gpkg.zip",
-            updateFrequency = "unknown",
-            update = TRUE)
+            updateFrequency = "unknown")
 
 regGeometry(gSeries = "gadm36",
             label = list(al1 = "NAME_0", al2 = "NAME_1", al3 = "NAME_2", al4 = "NAME_3", al5 = "NAME_4", al6 = "NAME_5"),
             archive = "gadm36_levels_gpkg.zip|gadm36_levels.gpkg",
             archiveLink = "https://geodata.ucdavis.edu/gadm/gadm4.1/gadm36_levels_gpkg.zip",
-            updateFrequency = "unknown",
-            update = TRUE)
+            updateFrequency = "unknown")
 
 
 # normalise geometries ----
