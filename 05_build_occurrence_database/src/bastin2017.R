237113ba4b0394a74dec99e508db88e5d98fe50c
EhrmannS <steffen.ehrmann@posteo.de>
2024-02-13 02:20:29 +0100

    set this up according to new template

--- 05_build_occurrence_database/src/bastin2017.R
+++ 05_build_occurrence_database/src/bastin2017.R
@@ -2,15 +2,17 @@
 #
 thisDataset <- "Bastin2017"
 description <- "The extent of forest in dryland biomes"
-url <- "https://doi.org/10.1126/science.aam6527 https://"
-licence <- INSERT
+doi <- "https://doi.org/10.1126/science.aam6527 https://"
+licence <- "unknown"
 
 message("\n---- ", thisDataset, " ----")
 
 
 # reference ----
 #
-bib <- bibtex_reader(paste0(occurr_dir, "input/", thisDataset, "/", "csp_356_.bib"))
+bib <- read.bib(file = paste0(occurr_dir, "input/", thisDataset, "/", "csp_356_.bib"))
+newBib <- read.bib(file = paste0(occurr_dir, "references.bib")) %>% c(bib)
+newBib <- newBib[!duplicated(newBib)]
 
 
 # read dataset ----
@@ -21,71 +23,40 @@ data_path <- paste0(occurr_dir, "input/", thisDataset, "/", "aam6527_Bastin_Data
 # (unzip/untar)
 unzip(exdir = paste0(occurr_dir, "input/", thisDataset, "/"), zipfile = data_path_comp)
 
-unzip(exdir = paste0(occurrenceDBDir, "00_incoming/", thisDataset, "/"),
-      zipfile = paste0(occurrenceDBDir, "00_incoming/", thisDataset, "/", ))
-
 data <- read_delim(file = data_path,
                    col_names = FALSE,
                    col_types = cols(.default = "c"),
                    delim = ";")
 
 
-# data management ----
-#
-# temp <- data %>%
-#   mutate(
-#     datasetID = thisDataset,
-#     fid = row_number(),
-#     type = "point",
-#     country = dryland_assessment_region,
-#     x = location_x,
-#     y = location_y,
-#     geometry = NA,
-#     epsg = 4326,
-#     area = NA_real_,
-#     date = ymd("2015", truncated = 2),
-#     externalID = NA_character_,
-#     externalValue = land_use_category,
-#     irrigated = FALSE,
-#     presence = if_else(land_use_category == "forest", TRUE, FALSE),
-#     sample_type = "visual interpretation",
-#     collector = "citizen scientist",
-#     purpose = "study") %>%
-#   select(datasetID, fid, type, country, x, y, geometry, epsg, area, date,
-#          externalID, externalValue, irrigated, presence,
-#          sample_type, collector, purpose, everything())
-
-
-
 # harmonise data ----
 #
 data <- data %>%
-  mutate(obsID = row_number(), .before = 1)                                     # define observation ID on raw data to be able to join harmonised data with the rest
+  mutate(obsID = row_number()-1, .before = 1) %>%
+  mutate(present = if_else(X5 == "forest", TRUE, FALSE))
 
-schema_INSERT <-
-  setFormat(decimal = INSERT, thousand = INSERT, na_values = INSERT) %>%
-  setIDVar(name = "datasetID", value = thisDataset) %>%                         # the dataset ID
-  setIDVar(name = "obsID", columns = 1) %>%                                     # the observation ID
-  setIDVar(name = "externalID", columns = INSERT) %>%                           # the verbatim observation-specific ID as used in the external dataset
-  setIDVar(name = "open", value = INSERT) %>%                                   # whether the dataset is freely available (TRUE) or restricted (FALSE)
-  setIDVar(name = "type", value = INSERT) %>%                                   # whether the data are "point" or "areal" (when its from a plot, region, nation, etc)
-  setIDVar(name = "x", columns = INSERT) %>%                                    # the x-value of the coordinate (or centroid if type = "areal")
-  setIDVar(name = "y", columns = INSERT) %>%                                    # the y-value of the coordinate (or centroid if type = "areal")
-  setIDVar(name = "epsg", value = INSERT) %>%                                   # the EPSG code of the coordinates or geometry
-  setIDVar(name = "geometry", columns = INSERT) %>%                             # the geometries if type = "areal"
-  setIDVar(name = "date", columns = INSERT) %>%                                 # the date of the observation
-  setIDVar(name = "sample_type", value = INSERT) %>%                            # from what space the data were collected, either "field/ground", "visual interpretation", "experience", "meta study" or "modelled"
-  setIDVar(name = "collector", value = INSERT) %>%                              # who the collector was, either "expert", "citizen scientist" or "student"
-  setIDVar(name = "purpose", value = INSERT) %>%                                # what the data were collected for, either "monitoring", "validation", "study" or "map development"
-  setObsVar(name = "value", columns = INSERT) %>%                               # the value of the observation
-  setObsVar(name = "irrigated", columns = INSERT) %>%                           # whether the observation receives irrigation (TRUE) or not (FALSE)
-  setObsVar(name = "present", columns = INSERT) %>%                             # whether the observation describes a presence (TRUE) or an absence (FALSE)
-  setObsVar(name = "area", columns = INSERT)                                    # the area covered by the observation (if type = "areal")
+schema_bastin2017 <-
+  setFormat(decimal = ".") %>%
+  setIDVar(name = "datasetID", value = thisDataset) %>%
+  setIDVar(name = "obsID", type = "i", columns = 1) %>%
+  setIDVar(name = "open", type = "l", value = TRUE) %>%
+  setIDVar(name = "type", value = "point") %>%
+  setIDVar(name = "x", type = "n", columns = 2) %>%
+  setIDVar(name = "y", type = "n", columns = 3) %>%
+  setIDVar(name = "epsg", value = "4326") %>%
+  setIDVar(name = "date", type = "D", value = ymd("2015", truncated = 2)) %>%
+  setIDVar(name = "irrigated", type = "l", value = FALSE) %>%
+  setIDVar(name = "present", type = "l", columns = 8) %>%
+  setIDVar(name = "sample_type", value = "visual interpretation") %>%
+  setIDVar(name = "collector", value = "citizen scientist") %>%
+  setIDVar(name = "purpose", value = "study") %>%
+  setObsVar(name = "concept", type = "c", columns = 6)
 
-temp <- reorganise(schema = schema_INSERT, input = data)
+temp <- reorganise(schema = schema_bastin2017, input = data)
 
 otherData <- data %>%
-  select(INSERT)                                                                # remove all columns that are recorded in 'out'
+  slice(-1) %>%
+  select(obsID, region = X3, aridity = X4, cover.tree = X6)
 
 
 # harmonize with ontology ----
@@ -98,21 +69,18 @@ new_source(name = thisDataset,
            ontology = onto_path)
 
 out <- matchOntology(table = temp,
-                     columns = "value",
+                     columns = "concept",
+                     colsAsClass = FALSE,
                      dataseries = thisDataset,
                      ontology = onto_path)
 
 
 # write output ----
 #
-validateFormat(object = out) %>%
-  saveRDS(file = paste0(occurr_dir, thisDataset, ".rds"))
+saveRDS(object = out, file = paste0(occurr_dir, "output/", thisDataset, ".rds"))
+saveRDS(object = otherData, file = paste0(occurr_dir, "output/", thisDataset, "_other.rds"))
 
-saveRDS(object = otherData, file = paste0(occurr_dir, thisDataset, "_other.rds"))
-
-read_lines(file = paste0(occurr_dir, "references.bib")) %>%
-  c(bibtex_writer(z = bib, key = thisDataset)) %>%
-  write_lines(file = paste0(occurr_dir, "references.bib"))
+write.bib(entry = newBib, file = paste0(occurr_dir, "references.bib"))
 
 
 # beep(sound = 10)
