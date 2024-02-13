4002321a4f3489e3b660f688d7a7c1272f095d02
EhrmannS <steffen.ehrmann@posteo.de>
2024-02-13 02:20:34 +0100

    new script

---
+++ 05_build_occurrence_database/src/stanimirova2023.R
@@ -0,0 +1,101 @@
+# script arguments ----
+#
+thisDataset <- INSERT                                                           # the ID of this dataset
+description <- INSERT                                                           # the abstract (if paper available) or project description
+doi <- INSERT                                                                   # either the doi to the dataset, the doi to the paper or the url to the dataset
+licence <- INSERT                                                               # the url to a license
+
+message("\n---- ", thisDataset, " ----")
+
+
+# reference ----
+#
+bib <- ris_reader(paste0(occurr_dir, "input/", thisDataset, "/", INSERT))       # in case of ris format
+bib <- bibtex_reader(paste0(occurr_dir, "input/", thisDataset, "/", INSERT))    # in case of bib format
+
+
+# read dataset ----
+#
+# data_path_comp <- paste0(occurr_dir, "input/", thisDataset, "/", "")
+data_path <- paste0(occurr_dir, "input/", thisDataset, "/", INSERT)
+
+# (unzip/untar)
+# unzip(exdir = data_path_comp, zipfile = data_path)                            # in case of zip archive
+# untar(exdir = data_path_comp, tarfile = data_path)                            # in case of tar archive
+
+data <- read_csv(file = data_path,
+                 col_names = FALSE,
+                 col_types = cols(.default = "c"))                              # in case of csv
+data <- read_tsv()
+data <- st_read(dsn = data_path) %>% as_tibble()                                # in case of geopackage/shape/...
+data <- read_excel(path = data_path)                                            # in case of excel file
+
+
+# data management ----
+#
+
+
+
+# harmonise data ----
+#
+data <- data %>%
+  mutate(obsID = row_number(), .before = 1)                                     # define observation ID on raw data to be able to join harmonised data with the rest
+
+schema_INSERT <-
+  setFormat(decimal = INSERT, thousand = INSERT, na_values = INSERT) %>%
+  setIDVar(name = "datasetID", value = thisDataset) %>%                         # the dataset ID
+  setIDVar(name = "obsID", columns = 1) %>%                                     # the observation ID
+  setIDVar(name = "externalID", columns = INSERT) %>%                           # the verbatim observation-specific ID as used in the external dataset
+  setIDVar(name = "open", value = INSERT) %>%                                   # whether the dataset is freely available (TRUE) or restricted (FALSE)
+  setIDVar(name = "type", value = INSERT) %>%                                   # whether the data are "point" or "areal" (when its from a plot, region, nation, etc)
+  setIDVar(name = "x", columns = INSERT) %>%                                    # the x-value of the coordinate (or centroid if type = "areal")
+  setIDVar(name = "y", columns = INSERT) %>%                                    # the y-value of the coordinate (or centroid if type = "areal")
+  setIDVar(name = "epsg", value = INSERT) %>%                                   # the EPSG code of the coordinates or geometry
+  setIDVar(name = "geometry", columns = INSERT) %>%                             # the geometries if type = "areal"
+  setIDVar(name = "date", columns = INSERT) %>%                                 # the date of the observation
+  setIDVar(name = "sample_type", value = INSERT) %>%                            # from what space the data were collected, either "field/ground", "visual interpretation", "experience", "meta study" or "modelled"
+  setIDVar(name = "collector", value = INSERT) %>%                              # who the collector was, either "expert", "citizen scientist" or "student"
+  setIDVar(name = "purpose", value = INSERT) %>%                                # what the data were collected for, either "monitoring", "validation", "study" or "map development"
+  setObsVar(name = "value", columns = INSERT) %>%                               # the value of the observation
+  setObsVar(name = "irrigated", columns = INSERT) %>%                           # whether the observation receives irrigation (TRUE) or not (FALSE)
+  setObsVar(name = "present", columns = INSERT) %>%                             # whether the observation describes a presence (TRUE) or an absence (FALSE)
+  setObsVar(name = "area", columns = INSERT)                                    # the area covered by the observation (if type = "areal")
+
+temp <- reorganise(schema = schema_INSERT, input = data)
+
+otherData <- data %>%
+  select(INSERT)                                                                # remove all columns that are recorded in 'out'
+
+
+# harmonize with ontology ----
+#
+new_source(name = thisDataset,
+           description = description,
+           homepage = doi,
+           date = ymd(INSERT),                                                        # the download date
+           license = licence,
+           ontology = onto_path)
+
+out <- matchOntology(table = temp,
+                     columns = "value",
+                     dataseries = thisDataset,
+                     ontology = onto_path)
+
+
+# write output ----
+#
+validateFormat(object = out) %>%
+  saveRDS(file = paste0(occurr_dir, thisDataset, ".rds"))
+
+saveRDS(object = otherData, file = paste0(occurr_dir, thisDataset, "_other.rds"))
+
+read_lines(file = paste0(occurr_dir, "references.bib")) %>%
+  c(bibtex_writer(z = bib, key = thisDataset)) %>%
+  write_lines(file = paste0(occurr_dir, "references.bib"))
+
+
+# beep(sound = 10)
+message("\n     ... done")
+
+
+
