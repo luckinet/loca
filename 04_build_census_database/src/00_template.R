a6b371ee85aa7816bc7fbd58f716f0d37b0f31bd
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-02-28 18:44:01 +0100

    revise

--- 04_build_census_database/src/00_template.R
+++ 04_build_census_database/src/00_template.R
@@ -1,115 +1,154 @@
 # script arguments ----
 #
-thisNation <- ""
-
-ds <- c("")
-gs <- c("")
+thisNation <- _INSERT
 
 
-# 1. register dataseries ----
+# 1. dataseries ----
 #
-# regDataseries(name = ds[],
-#               description = "",
-#               homepage = "",
-#               licence_link = "",
-#               licence_path = "")
+ds <- c(_INSERT)
+gs <- c(_INSERT)
+
+regDataseries(name = ds[],
+              description = _INSERT,
+              homepage = _INSERT,
+              version = _INSERT,
+              licence_link = _INSERT)
 
 
-# 2. register geometries ----
+# 2. geometries ----
 #
-# regGeometry(nation = !!thisNation, # or any other "class = value" combination from the gazetteer
-#             gSeries = gs[],
-#             label = list(al_ = ""),
-#             archive = "|",
-#             archiveLink = "",
-#             nextUpdate = "",
-#             updateFrequency = "",
-#             update = updateTables)
+regGeometry(nation = !!thisNation, # or any other "class = value" combination from the gazetteer
+            gSeries = gs[],
+            label = list(al_ = ""),
+            archive = "|",
+            archiveLink = _INSERT,
+            downloadDate = _INSERT,
+            updateFrequency = _INSERT)
 
 
-# 3. register census tables ----
+# 3. tables ----
 #
-# schema_crops <- setCluster() %>%
-#   setFormat() %>%
-#   setIDVar(name = "al2", ) %>%
-#   setIDVar(name = "al3", ) %>%
-#   setIDVar(name = "year", ) %>%
-#   setIDVar(name = "methdod", value = "") %>%
-#   setIDVar(name = "crop", ) %>%
-#   setObsVar(name = "harvested", unit = "ha", ) %>%
-#   # for livestock
-#   setIDVar(name = "animal", )  %>%
-#   setObsVar(name = "headcount", unit = "n", ) %>%
-#   # for landuse
-#   setIDVar(name = "landuse", ) %>%
-#   setObsVar(name = "area", unit = "ha", )
-#
-#
-# regTable(nation = !!thisNation, # or any other "class = value" combination from the gazetteer
-#          label = "al_", # this should be the name of a level in the gazetteer
-#          subset = "",
-#          dSeries = ds[],
-#          gSeries = gs[],
-#          schema = ,
-#          begin = , # first year in the table
-#          end = , # last year in the table
-#          archive = "", # the name of this table in our database
-#          archiveLink = "", # where this table can be found online
-#          updateFrequency = "",
-#          nextUpdate = "",
-#          metadataPath = "",
-#          metadataLink = "")
-
 if(build_crops){
   ## crops ----
 
+  schema_crops <- setCluster() %>%
+    setFormat() %>%
+    setIDVar(name = "al2", ) %>%
+    setIDVar(name = "al3", ) %>%
+    setIDVar(name = "year", ) %>%
+    setIDVar(name = "methdod", value = "") %>%
+    setIDVar(name = "crop", ) %>%
+    setObsVar(name = "harvested", )
+
+  regTable(nation = !!thisNation, # or any other "class = value" combination from the gazetteer
+           label = "al_", # this should be the name of a level in the gazetteer
+           subset = _INSERT,
+           dSeries = ds[],
+           gSeries = gs[],
+           schema = schema_crops,
+           begin = _INSERT, # first year in the table
+           end = _INSERT, # last year in the table
+           archive = _INSERT, # the name of this table in our database
+           archiveLink = _INSERT, # where this table can be found online
+           updateFrequency = _INSERT,
+           nextUpdate = _INSERT,
+           metadataPath = _INSERT,
+           metadataLink = _INSERT)
+
+  normTable(pattern = ds[],
+            ontoMatch = "crop",
+            beep = 10)
 }
 
 if(build_livestock){
   ## livestock ----
 
+  schema_livestock <- setCluster() %>%
+    setFormat() %>%
+    setIDVar(name = "al2", ) %>%
+    setIDVar(name = "al3", ) %>%
+    setIDVar(name = "year", ) %>%
+    setIDVar(name = "methdod", value = "") %>%
+    setIDVar(name = "animal", )  %>%
+    setObsVar(name = "headcount", )
+
+  regTable(nation = !!thisNation,
+           label = "al_",
+           subset = _INSERT,
+           dSeries = ds[],
+           gSeries = gs[],
+           schema = schema_livestock,
+           begin = _INSERT,
+           end = _INSERT,
+           archive = _INSERT,
+           archiveLink = _INSERT,
+           updateFrequency = _INSERT,
+           nextUpdate = _INSERT,
+           metadataPath = _INSERT,
+           metadataLink = _INSERT)
+
+  normTable(pattern = ds[],
+            ontoMatch = "animal",
+            beep = 10)
 }
 
 if(build_landuse){
   ## landuse ----
 
+  schema_landuse <- setCluster() %>%
+    setFormat() %>%
+    setIDVar(name = "al2", ) %>%
+    setIDVar(name = "al3", ) %>%
+    setIDVar(name = "year", ) %>%
+    setIDVar(name = "methdod", value = "") %>%
+    setIDVar(name = "landuse", ) %>%
+    setObsVar(name = "area", )
+
+  regTable(nation = !!thisNation,
+           label = "al_",
+           subset = _INSERT,
+           dSeries = ds[],
+           gSeries = gs[],
+           schema = schema_landuse,
+           begin = _INSERT,
+           end = _INSERT,
+           archive = _INSERT,
+           archiveLink = _INSERT,
+           updateFrequency = _INSERT,
+           nextUpdate = _INSERT,
+           metadataPath = _INSERT,
+           metadataLink = _INSERT)
+
+  normTable(pattern = ds[],
+            ontoMatch = "landuse",
+            beep = 10)
 }
 
 
 #### test schemas
-
-myRoot <- paste0(census_dir, "tables/stage2/")
-myFile <- ""
-input <- read_csv(file = paste0(myRoot, myFile),
-                  col_names = FALSE,
-                  col_types = cols(.default = "c"))
-
-schema <-
-validateSchema(schema = schema, input = input)
-
-output <- reorganise(input = input, schema = schema)
-
+#
+# myRoot <- paste0(census_dir, "tables/stage2/")
+# myFile <- ""
+# input <- read_csv(file = paste0(myRoot, myFile),
+#                   col_names = FALSE,
+#                   col_types = cols(.default = "c"))
+#
+# schema <-
+# validateSchema(schema = schema, input = input)
+#
+# output <- reorganise(input = input, schema = schema)
+#
 # https://github.com/luckinet/tabshiftr/issues
 #### delete this section after finalising script
 
 
 # 4. normalise geometries ----
 #
-# only needed if GADM basis has not been built before
-# normGeometry(pattern = "gadm",
-#              outType = "gpkg")
-
 # normGeometry(pattern = gs[],
 #              outType = "gpkg")
 
 
 # 5. normalise census tables ----
 #
-## in case the output shall be examined before writing into the DB
-# testing <- normTable(nation = thisNation,
-#                      update = FALSE,
-#                      keepOrig = TRUE)
-
 # normTable(pattern = ds[],
-#           ontoMatch = ,
-#           outType = "rds")
+#           ontoMatch = )
