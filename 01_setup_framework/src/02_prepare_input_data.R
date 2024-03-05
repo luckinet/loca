07d629f1e6219bdc93ed466d6511ead8c73e759a
EhrmannS <steffen.ehrmann@posteo.de>
2024-03-06 00:31:27 +0100

    rename

---
+++ 01_setup_framework/src/02_prepare_input_data.R
@@ -0,0 +1,11 @@
+message("\n---- prepare input data ----")
+
+
+# prepare territorial basis ----
+#
+message(" --> downloading GADM")
+
+
+# prepare landcover basis ----
+#
+message(" --> downloading ESA-CCI landcover")
--- 01_setup_framework/src/03_prepare_input_data.R
+++
@@ -1,5 +0,0 @@
-# script description ----
-#
-# This is the script that prepares all input data that are not deliberately
-# handled in other modules.
-
