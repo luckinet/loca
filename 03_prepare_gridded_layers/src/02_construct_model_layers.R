516f93e0fe7b83bf2af232ac0598547323d2aed9
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-03-06 11:34:29 +0100

    refine

--- 03_prepare_gridded_layers/src/02_construct_model_layers.R
+++ 03_prepare_gridded_layers/src/02_construct_model_layers.R
@@ -1,12 +1,18 @@
 message("\n---- construct model-specific layers ----")
 
 
-world_template <- rast(res = model_info$parametes$pixel_size[1], vals = 1)
+# derive input objects ----
+#
+if(!exists("world_template")){
+        world_template <- rast(res = model_info$parametes$pixel_size[1], vals = 1)
+}
+
+model_vect <- vect(ext(model_info$parametes$extent), crs = crs(world_template))
 
 # derive model mask ----
 #
 message(" --> model mask")
-mask(x = world_template, mask = vect(ext(model_info$parametes$extent), crs = crs(world_template)),
+mask(x = world_template, mask = model_vect,
      filename = path_modelregion,
      overwrite = TRUE,
      filetype = "GTiff",
