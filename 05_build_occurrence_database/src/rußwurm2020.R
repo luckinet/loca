21e66dabdd92399e348ba3daacf892e0884e944f
EhrmannS <steffen.ehrmann@posteo.de>
2024-04-24 19:21:09 +0200

    fnialize

--- 05_build_occurrence_database/src/rußwurm2020.R
+++ 05_build_occurrence_database/src/rußwurm2020.R
@@ -1,17 +1,16 @@
 # ----
 # geography : France
-# period    : 2006 - 2022
+# period    : 2017 - 2018
 # typology  :
-#   - cover  : _INSERT
-#   - dynamic: _INSERT
-#   - use    : _INSERT
-# features  : _INSERT
+#   - cover  : VEGETATED
+#   - use    : crops
+# features  : -
 # data type : areal
-# doi/url   : https://doi.org/10.5194/isprs-archives-XLIII-B2-2020-1545-2020, https://geoservices.ign.fr/rpg
+# doi/url   : https://doi.org/10.5194/isprs-archives-XLIII-B2-2020-1545-2020
 # authors   : Steffen Ehrmann
 # date      : 2024-04-24
-# status    : inventarize, validate, normalize, done
-# comment   : the data source mentioned in the paper was scrutinized for further data, which were then downloaded and processed from their raw state (incl. our own harmonization), replacing the data used in the paper.
+# status    : done
+# comment   : the data source mentioned in the paper was scrutinized for further data and it turns out that these are the same data as in schneider2023. Since I need to harmonise everything with the LUCKINet ontology, I download the raw data and handle them in rpgfrance.R
 # ----
 
 thisDataset <- "rußwurm2020"
@@ -22,100 +21,3 @@ message(" --> reading in data")
 input_dir <- paste0(occurr_dir, "input/", thisDataset, "/")
 
 bib <- read.bib(file = paste0(input_dir, "isprs-archives-XLIII-B2-2020-1545-2020.bib"))
-# bib <- bibtex_reader(paste0(occurrenceDBDir, "00_incoming/", thisDataset, "/", "abs-1905-11893.bib"))
-
-# data_path_cmpr <- paste0(input_dir, "")
-# unzip(exdir = input_dir, zipfile = data_path_cmpr)
-# untar(exdir = input_dir, tarfile = data_path_cmpr)
-
-# untar(exdir = occurrenceDBDir, "00_incoming/", thisDataset, tarfile = paste0(occurrenceDBDir, "00_incoming/", thisDataset, "belle-ile.tar.gz"))
-#
-# labels <- read_delim(paste0(occurrenceDBDir, "00_incoming/", thisDataset, "/", "codes.csv"), delim = ";") %>%
-#   rename(CODE_CULTU = `Code Culture`)
-# data <- st_read(dsn = paste0(occurrenceDBDir, "00_incoming/", thisDataset, "/", "belle-ile.shp")) %>%
-#   left_join(labels, by = "CODE_CULTU")
-#
-# centroids <- data %>%
-#   st_centroid() %>%
-#   st_coordinates() %>%
-#   as_tibble()
-# areas <- data %>%
-#   st_area() %>%
-#   as.numeric()
-# data <- data %>%
-#   as_tibble()
-
-data_path <- paste0(input_dir, _INSERT)
-data <- read_csv(file = data_path)
-data <- read_tsv(file = data_path)
-data <- read_excel(path = data_path)
-data <- read_parquet(file = data_path)
-data <- st_read(dsn = data_path) %>% as_tibble()
-
-
-message(" --> normalizing data")
-data <- data %>%
-  mutate(obsID = row_number(), .before = 1)
-
-other <- data %>%
-  select(obsID, _INSERT)
-
-schema_INSERT <-
-  setFormat(header = _INSERT, decimal = _INSERT, thousand = _INSERT,
-            na_values = _INSERT) %>%
-  setIDVar(name = "datasetID", value = thisDataset) %>%
-  setIDVar(name = "obsID", type = "i", columns = 1) %>%
-  setIDVar(name = "externalID", columns = _INSERT) %>% # ID
-  setIDVar(name = "open", type = "l", value = _INSERT) %>%
-  setIDVar(name = "type", value = "areal") %>%
-  setIDVar(name = "x", type = "n", columns = _INSERT) %>%
-  setIDVar(name = "y", type = "n", columns = _INSERT) %>%
-  setIDVar(name = "geometry", columns = _INSERT) %>%
-  setIDVar(name = "date", columns = _INSERT) %>%
-  setIDVar(name = "irrigated", type = "l", value = _INSERT) %>%
-  setIDVar(name = "present", type = "l", value = TRUE) %>%
-  setIDVar(name = "sample_type", value = "field") %>%
-  setIDVar(name = "collector", value = "expert") %>%
-  setIDVar(name = "purpose", value = "validation") %>%
-  setObsVar(name = "concept", type = "c", columns = _INSERT) # `Libellé Culture`
-
-temp <- reorganise(schema = schema_INSERT, input = data)
-
-
-message(" --> harmonizing with ontology")
-new_source(name = thisDataset,
-           description = "We present BreizhCrops, a novel benchmark dataset for the supervised classification of field crops from satellite time series. We aggregated label data and Sentinel-2 top-of-atmosphere as well as bottom-of-atmosphere time series in the region of Brittany (Breizh in local language), north-east France. We compare seven recently proposed deep neural networks along with a Random Forest baseline. The dataset, model (re-)implementations and pre-trained model weights are available at the associated GitHub repository (https://github.com/dl4sits/breizhcrops) that has been designed with applicability for practitioners in mind. We plan to maintain the repository with additional data and welcome contributions of novel methods to build a state-of-the-art benchmark on methods for crop type mapping.",
-           homepage = "https://doi.org/10.5194/isprs-archives-XLIII-B2-2020-1545-2020",
-           date = ymd("2020-06-08"),
-           license = "https://www.gnu.org/licenses/gpl-3.0.txt",
-           ontology = odb_onto_path)
-
-# matches <-
-#   tibble(new = unique(data$`Libellé Culture`),
-#          old = c("Permanent grazing", "Permanent grazing", "potato", "Fallow",
-#                  "Tree orchards", "Temporary grazing", NA, "cabbage", "barley",
-#                  "maize", "sugarbeet", "sorghum", "rapeseed", NA, "strawberry",
-#                  "melon", "pumpkin", "leek", "Medicinal crops",
-#                  "Root or Bulb vegetables", "Grass crops", "Grass crops",
-#                  "Grass crops", "FODDER CROPS", "oat", "alfalfa", "oat",
-#                  "wheat", "triticale", "Fallow", "Fallow", "barley",
-#                  "Other fodder crops", "Grazing Woodland", "Mixed cereals",
-#                  "Fodder legumes", "Fallow", "Permanent grazing", "soybean",
-#                  "Fallow", "Medicinal crops", "Woody plantation", "wheat",
-#                  "maize", "Fodder legumes", "broad bean"))
-
-out <- matchOntology(table = temp,
-                     columns = "concept",
-                     colsAsClass = FALSE,
-                     dataseries = thisDataset,
-                     ontology = odb_onto_path)
-
-out <- list(harmonised = out, extra = other)
-
-
-message(" --> writing output")
-saveRDS(object = out, file = paste0(dir_occurr, "output/", thisDataset, ".rds"))
-saveBIB(object = bib, file = paste0(dir_occurr, "references.bib"))
-
-beep(sound = 10)
-message("\n     ... done")
--- 05_build_occurrence_database/src/schneider2023.R
+++ 05_build_occurrence_database/src/schneider2023.R
@@ -1,107 +1,25 @@
 # ----
 # geography : Europe
-# period    : _INSERT
+# period    : 2020 - 2021
 # typology  :
-#   - cover  : _INSERT
-#   - dynamic: _INSERT
-#   - use    : _INSERT
-# features  : _INSERT
+#   - cover  : VEGETATED
+#   - use    : crops
+# features  : -
 # sample    : _INSERT
-# doi/url   : _INSERT
-# license   : _INSERT
-# disclosed : _INSERT
-# doi/url   : https://github.com/maja601/EuroCrops
+# doi/url   : https://doi.org/10.1038/s41597-023-02517-0, https://github.com/maja601/EuroCrops
+# license   : https://creativecommons.org/licenses/by-sa/4.0/
+# disclosed : yes
 # authors   : Steffen Ehrmann
-# date      : 2024-MM-DD
+# date      : 2024-04-24
 # status    : find data, update, inventarize, validate, normalize, done
-# comment   : _INSERT
+# comment   : since we are building an even bigger "Harmonized Open Crop Dataset" and not only across Europe, we need to harmonize the labels with the LUCKINet ontology and therefore download and process each dataset again from the raw data.
 # ----
 
-thisDataset <- "eurocrops"
+thisDataset <- "schneider2023"
 message("\n---- ", thisDataset, " ----")
-
-# current data repositories
-# https://github.com/maja601/EuroCrops
-#
-# OR
-#
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2021pteurocroppoly110
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2021denrweurocroppoly110
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2021lteurocroppoly110
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2021sieurocroppoly110
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2021eeeurocroppoly110
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2021delseurocroppoly110
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2021seeurocroppoly110
-# https://worldcereal-rdm.geo-wiki.org/collections/details/?id=2020nleurocroppoly110
 
 
 message(" --> reading in data")
 input_dir <- paste0(dir_occurr, "input/", thisDataset, "/")
 
 bib <- read.bib(file = paste0(input_dir, "10.1038_s41597-023-02517-0-citation.bib"))
-# bib <- bibtex_reader(paste0(occurrenceDBDir, "00_incoming/", thisDataset, "/", "ref.bib"))
-
-# data_path_cmpr <- paste0(input_dir, "")
-data_path <- paste0(input_dir, "_INSERT")
-
-# (unzip/untar)
-# unzip(exdir = input_dir, zipfile = data_path_cmpr)
-# untar(exdir = input_dir, tarfile = data_path_cmpr)
-
-data <- read_csv(file = data_path)
-data <- read_tsv(file = data_path)
-data <- st_read(dsn = data_path) %>% as_tibble()
-data <- read_excel(path = data_path)
-
-
-message(" --> normalizing data")
-data <- data %>%
-  mutate(obsID = row_number(), .before = 1)
-
-other <- data %>%
-  select(_INSERT)
-
-schema_INSERT <-
-  setFormat(header = _INSERT, decimal = _INSERT, thousand = _INSERT,
-            na_values = _INSERT) %>%
-  setIDVar(name = "datasetID", value = thisDataset) %>%
-  setIDVar(name = "obsID", type = "i", columns = 1) %>%
-  setIDVar(name = "externalID", columns = _INSERT) %>%
-  setIDVar(name = "open", type = "l", value = _INSERT) %>%
-  setIDVar(name = "type", value = _INSERT) %>%
-  setIDVar(name = "x", type = "n", columns = _INSERT) %>%
-  setIDVar(name = "y", type = "n", columns = _INSERT) %>%
-  setIDVar(name = "geometry", columns = _INSERT) %>%
-  setIDVar(name = "date", columns = _INSERT) %>%
-  setIDVar(name = "irrigated", type = "l", value = _INSERT) %>%
-  setIDVar(name = "present", type = "l", value = _INSERT) %>%
-  setIDVar(name = "sample_type", value = _INSERT) %>%
-  setIDVar(name = "collector", value = _INSERT) %>%
-  setIDVar(name = "purpose", value = _INSERT) %>%
-  setObsVar(name = "concept", type = "c", columns = _INSERT)
-
-temp <- reorganise(schema = schema_INSERT, input = data)
-
-
-message(" --> harmonizing with ontology")
-new_source(name = thisDataset,
-           description = _INSERT,
-           homepage = "https://github.com/maja601/EuroCrops",
-           date = ymd("2022-04-27"),
-           license = "https://creativecommons.org/licenses/by/4.0/",
-           ontology = odb_onto_path)
-
-out <- matchOntology(table = temp,
-                     columns = "concept",
-                     colsAsClass = FALSE,
-                     dataseries = thisDataset,
-                     ontology = odb_onto_path)
-
-
-message(" --> writing output")
-saveRDS(object = out, file = paste0(occurr_dir, "output/", thisDataset, ".rds"))
-saveRDS(object = other, file = paste0(occurr_dir, "output/", thisDataset, "_extra.rds"))
-saveBIB(object = bib, file = paste0(occurr_dir, "references.bib"))
-
-beep(sound = 10)
-message("\n     ... done")
