bb9c7461d53da4159403fa6597a0955698ce5cbb
EhrmannS <steffen.ehrmann@posteo.de>
2024-04-26 12:24:51 +0200

    .

--- 05_build_occurrence_database/src/schneider2023.R
+++ 05_build_occurrence_database/src/schneider2023.R
@@ -6,13 +6,13 @@
 #   - use    : crops
 # features  : -
 # sample    : _INSERT
-# doi/url   : https://doi.org/10.1038/s41597-023-02517-0, https://github.com/maja601/EuroCrops
+# doi/url   : https://doi.org/10.1038/s41597-023-02517-0, https://doi.org/10.5281/zenodo.10118572
 # license   : https://creativecommons.org/licenses/by-sa/4.0/
 # disclosed : yes
 # authors   : Steffen Ehrmann
 # date      : 2024-04-24
-# status    : find data, update, inventarize, validate, normalize, done
-# comment   : since we are building an even bigger "Harmonized Open Crop Dataset" and not only across Europe, we need to harmonize the labels with the LUCKINet ontology and therefore download and process each dataset again from the raw data.
+# status    : done
+# comment   : as this database does not provide raw data and as we need to harmonize the labels with the LUCKINet ontology, we download and process each dataset again from the raw data if the download is still available, such as in the scripts "rpg_france.R" (from source), "lpis_slovenia.R" (from source), "lpis_estonia.R" (2023 from source), "lpis_czechia" (from source), "lpis_denmark.R" (from source), "lpis_croatia.R" (from source), "lpis_austria.R" (from source) and "".
 # ----
 
 thisDataset <- "schneider2023"
@@ -23,3 +23,7 @@ message(" --> reading in data")
 input_dir <- paste0(dir_occurr, "input/", thisDataset, "/")
 
 bib <- read.bib(file = paste0(input_dir, "10.1038_s41597-023-02517-0-citation.bib"))
+
+
+handle latvia 2021
+handle estonia 2021
