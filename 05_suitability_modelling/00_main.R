e38dd6e2b8b12f4d428e98c8e994552b93dd21fc
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-08-10 18:38:13 +0200

    move main code to 00_main.R and change folder and data structure within module

---
+++ 05_suitability_modelling/00_main.R
@@ -0,0 +1,23 @@
+# ----
+# title       : suitability modelling
+# description : This is the main script for suitability modelling within LOCA
+# license     : https://creativecommons.org/licenses/by-sa/4.0/
+# authors     : Julián Equihua, Steffen Ehrmann
+# date        : 2024-MM-DD
+# version     : 0.2.0
+# status      : working
+# comment     : file.edit(paste0(dir_docs, "/documentation/06_suitability_modelling.md"))
+# ----
+
+# set module-specific paths ----
+#
+
+
+# run scripts (old scripts) ----
+#
+# source(paste0(dir_mdl06, "src/01_sample_covariates.R"))
+# source(paste0(dir_mdl06, "src/02_impute_pseudoAbsences.R"))
+# source(paste0(dir_mdl06, "src/03_randomForest.R"))
+# source(paste0(dir_mdl06, "src/04_prediction.R"))
+# source(paste0(dir_mdl06, "src/05_postprocessing.R"))
+# source(paste0(dir_mdl06, "src/99_test-output.R"))
