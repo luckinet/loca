6e41e4b74b364f58829b230170f730e629812590
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2024-08-10 18:38:23 +0200

    move main code to 00_main.R and change folder and data structure within module

---
+++ 07_allocation_modelling/00_main.R
@@ -0,0 +1,14 @@
+# ----
+# title       : allocation modelling
+# description : This is the main script for configuring and running CLUMondo to allocate land-use statistics in space and time.
+# license     : https://creativecommons.org/licenses/by-sa/4.0/
+# authors     : Steffen Ehrmann
+# date        : 2024-03-27
+# version     : 0.2.0
+# status      : work in progress
+# comment     : file.edit(paste0(dir_docs, "/documentation/08_allocation_modelling.md"))
+# ----
+
+# set module-specific paths ----
+#
+
